654dc4cc8ee757abb71c41d7b9431c4c1bf7c29c
AbdallahIsmaili <aismaili691@gmail.com>
2023-02-20 17:37:03 +0100

    update

---
+++ cogs/MyCog.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class MyCog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('MyCog.py is ready!')
+
+    @commands.command()
+    async def embed(self, ctx):
+        embed_message = discord.Embed(title="Title of embed", description="Description of embed", color=discord.Color.red())
+
+        embed_message.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar)
+        embed_message.set_thumbnail(url=ctx.guild.icon)
+        embed_message.set_image(url=ctx.guild.icon)
+        embed_message.add_field(name="Field name", value="Field value", inline=False)
+        embed_message.set_footer(text="This is the footer", icon_url=ctx.author.avatar)
+
+        await ctx.send(embed = embed_message)
+
+async def setup(client):
+    await client.add_cog(MyCog(client))
--- main.py
+++ main.py
@@ -1,40 +1,50 @@
-import random
-
+# import random
 import discord
-from discord.ext import commands, tasks
-from itertools import cycle
+from discord.ext import commands
+import os
+import asyncio
+# from discord.ext import tasks
+# from itertools import cycle
 
 client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
-bot_status = cycle(["Type in '$help' for help ", "Type in '$build' for characters builds", "Type in '$ping' for see your internet conection speed"])
+# bot_status = cycle(["Type in '$help' for help ", "Type in '$build' for characters builds", "Type in '$ping' for see your internet conection speed"])
 
-@tasks.loop(seconds=5)
-async def change_status():
-    await client.change_presence(activity = discord.Game(next(bot_status)))
+# @tasks.loop(seconds=5)
+# async def change_status():
+#     await client.change_presence(activity = discord.Game(next(bot_status)))
 
 @client.event
 async def on_ready():
     print("Success: Bot is connected to Discord")
-    change_status.start()
+    # change_status.start()
 
-@client.command()
-async def ping(ctx):
-    bot_latency = round(client.latency * 1000)
-    await ctx.send(f"Pong!, {bot_latency} ms.")
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"cogs.{filename[:-3]}")
 
-@client.command()
-async def hello(ctx):
-    await ctx.send("Hello there!")
-
-@client.command(aliases=["8ball", "eightball", "eight ball", "8 ball"])
-async def magic_eightball(ctx, *, question):
-    with open('responses.txt', 'r') as f:
-        random_responses = f.readlines()
-        response = random.choice(random_responses)
-
-    await ctx.send(response)
+async def main():
+    async with client:
+        await load()
+        await client.start("TOKEN")
 
 
+# @client.command()
+# async def ping(ctx):
+#     bot_latency = round(client.latency * 1000)
+#     await ctx.send(f"Pong!, {bot_latency} ms.")
 
+# @client.command()
+# async def hello(ctx):
+#     await ctx.send("Hello there!")
+#
+# @client.command(aliases=["8ball", "eightball", "eight ball", "8 ball"])
+# async def magic_eightball(ctx, *, question):
+#     with open('responses.txt', 'r') as f:
+#         random_responses = f.readlines()
+#         response = random.choice(random_responses)
+#
+#     await ctx.send(response)
 
-client.run("MTA3Njk5Mjk5NDc2MDIxMjQ5MQ.GeADUF.8G2lI93dRCjhmvmBtTzfBLRyjQqjzO2c3UphOw")
+asyncio.run(main())
