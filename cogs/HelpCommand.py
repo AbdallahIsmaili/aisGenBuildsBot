37555ea06d395422c82157c23fa7e062a0e91919
AbdallahIsmaili <aismaili691@gmail.com>
2023-03-26 22:07:32 +0000

    update

--- cogs/HelpCommand.py
+++ cogs/HelpCommand.py
@@ -35,5 +35,103 @@ class HelpCommand(commands.Cog):
 
         await interactions.response.send_message(embed=help_embed)
 
+    @commands.hybrid_command(name="test", description="This is test command")
+    async def test(self, ctx: commands.Context):
+        await ctx.send("/ayaka")
+
+
+
+
 async def setup(client):
     await client.add_cog(HelpCommand(client))
+
+
+
+
+
+
+
+
+
+# import discord
+# from discord import app_commands
+# from discord.ext import commands
+# from discord.ui import View, Select
+#
+#
+# class HelpSelect(Select):
+#     def __init__(self, bot: commands.Bot):
+#         super().__init__(
+#             placeholder="Choose a Category",
+#             options=[
+#                 discord.SelectOption(
+#                     label='Characters', description='Characters infos'
+#                 ) for cog_name, cog in bot.cogs.items() if cog.__cog_commands__ and cog_name not in ['Jishaku']
+#             ]
+#         )
+#
+#         self.bot = bot
+#
+#     async def callback(self, interaction: discord.Interaction) -> None:
+#         cog = self.bot.get_cog(self.values[0])
+#         assert cog
+#
+#         commands_mixer = []
+#         for i in cog.walk_commands():
+#             commands_mixer.append(i)
+#
+#         for i in cog.walk_app_commands():
+#             commands_mixer.append(i)
+#
+#         embed = discord.Embed(
+#             title=f"{cog.__cog_name__} Commands",
+#             description='\n'.join(
+#                 f"**{command.name}**: `{command.description}`"
+#                 for command in commands_mixer
+#             )
+#         )
+#         await interaction.response.send_message(
+#             embed=embed,
+#             ephemeral=True
+#         )
+#
+# class HelpCommand(commands.Cog):
+#     def __init__(self, bot):
+#         self.bot = bot
+#
+#     @commands.Cog.listener()
+#     async def on_ready(self):
+#         await self.bot.tree.sync()
+#         print('HelpCommand.py is ready!')
+#
+#     @commands.hybrid_command(name="help", description="قائمة المساعدة.")
+#     async def help(self, ctx: commands.Context):
+#
+#         help_embed = discord.Embed(title="__aiGenshin Builds | Help Directory__", description="Select a category from the dropdown to view commands specific to that category.", color=discord.Color.gold())
+#
+#         help_embed.set_author(name="aiGenshin_Builds")
+#
+#         help_embed.set_thumbnail(
+#             url="https://yoolk.ninja/wp-content/uploads/2021/08/Games-GenshinImpact-1024x1024.png")
+#
+#         help_embed.set_image(url="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/10/genshin-impact-beginning.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5")
+#
+#         help_embed.add_field(name="Categories",
+#                                 value=">  Builds \n > Characters \n > Weapons \n > Artifacts \n > Materials & Bosses",
+#                                 inline=False)
+#
+#         view = View().add_item(HelpSelect(self.bot))
+#
+#
+#         await ctx.send(embed=help_embed, view=view)
+#
+#
+#     @commands.hybrid_command(name="test", description="This is test command")
+#     async def test(self, ctx: commands.Context):
+#         await ctx.send(10)
+#
+#
+#
+#
+# async def setup(bot):
+#     await bot.add_cog(HelpCommand(bot))
--- main.py
+++ main.py
@@ -22,11 +22,86 @@ async def on_ready():
     print("Success: Bot is connected to Discord")
     change_status.start()
 
+
+
+#
+# class SelectMenu(discord.ui.View):
+#     def __init__(self):
+#         super().__init__()
+#         self.select = discord.ui.Select(placeholder="Choose a Category...", options=self.options)
+#         self.select.add_option(label="Characters", value="1", description="Characters infos")
+#         self.select.add_option(label="Builds", value="2", description="Characters Builds")
+#         self.select.add_option(label="Weapons", value="3", description="Weapons infos")
+#         self.select.add_option(label="Artifacts", value="4", description="Artifacts infos")
+#         self.select.add_option(label="Materials", value="5", description="Materials, Bosses and Domains")
+#         self.add_child(self.select)
+#
+#     async def on_select(self, interaction: discord.Interaction, select: discord.ui.Select,):
+#         if select.values[0] == "1":
+#             await interaction.response.send_message(content='Characters infos')
+#
+#         elif select.values[0] == "2":
+#             await interaction.response.send_message(content='Characters Builds')
+#
+#         elif select.values[0] == "3":
+#             await interaction.response.send_message(content='Weapons infos')
+#
+#         elif select.values[0] == "4":
+#             await interaction.response.send_message(content='Artifacts infos')
+#
+#         elif select.values[0] == "5":
+#             await interaction.response.send_message(content='Materials, Bosses and Domains')
+#
+# @client.tree.command(name="category", description="Choose a category")
+# async def category(interaction: discord.Interaction):
+#     select_menu = SelectMenu()
+#     await interaction.response.send_message(content="Select a category", view=select_menu)
+#     await select_menu.wait()
+
+
+
+
+
+
+class SelectTest(discord.ui.Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label="ayaka", description="ayaka build"),
+            discord.SelectOption(label="hutao", description="hutao build"),
+            discord.SelectOption(label="raiden shogun", description="raiden shogun build")
+        ]
+        super().__init__(placeholder="choose a character?", options=options, min_values=1, max_values=1)
+
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"{self.values[0]} build")
+
+class SelectView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(SelectTest())
+
+@client.tree.command(name="build", description="Choose a character")
+async def build(ctx: commands.Context):
+    await ctx.send("click on the dropdown below", view=SelectView())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @client.tree.command(name="ping", description="Show the bot's latency in ms.")
 async def ping(interaction: discord.Interaction):
     bot_latency = round(client.latency * 1000)
     await interaction.response.send_message(f"Pong! {bot_latency} ms.")
-
 
 async def load():
     for filename in os.listdir("./cogs"):
@@ -36,7 +111,7 @@ async def load():
 async def main():
     async with client:
         await load()
-        await client.start("TOKEN")
+        await client.start("MTA3Njk5Mjk5NDc2MDIxMjQ5MQ.GAMNjG.vbjOx3RJ2zVpDHBcgRZLZK3uNTJIbyJtDNgsdQ")
 
 
 
